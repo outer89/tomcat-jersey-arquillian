d0f8dc9c3288114eaca2b8efabf314826f56dca9
Niki Dokovski <nickytd@gmail.com>
2013-09-03 15:11:06 +0300

    test case optimizations based on feedback from Aslak Knutsen

--- src/test/java/example/MyResourceTest.java
+++ src/test/java/example/MyResourceTest.java
@@ -2,11 +2,13 @@ package example;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.GenericArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
 import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
+
 
 
 import javax.ws.rs.client.Client;
@@ -24,7 +26,8 @@ import java.util.logging.Logger;
 @RunWith(Arquillian.class)
 public class MyResourceTest
 {
-    private static final String MYRESOURCE_URL = "http://localhost:8888/webapi";
+    @ArquillianResource
+    private URL baseURL;
 
     // -------------------------------------------------------------------------------------||
     // Class Members -----------------------------------------------------------------------||
@@ -42,7 +45,7 @@ public class MyResourceTest
     /**
      * Define the deployment
      */
-    @Deployment
+    @Deployment(testable=false)
     public static WebArchive createTestArchive()
     {
         return ShrinkWrap
@@ -60,8 +63,10 @@ public class MyResourceTest
     @Test
     public void shouldBeAbleToInvokeServletInDeployedWebApp() throws Exception
     {
+    	
+    	
         Client c = ClientBuilder.newClient();
-        WebTarget target = c.target(MYRESOURCE_URL);
+        WebTarget target = c.target(baseURL+"/webapi");
         String responseMsg = target.path("myresource").request().get(String.class);
         Assert.assertEquals("Got it!", responseMsg);
 
