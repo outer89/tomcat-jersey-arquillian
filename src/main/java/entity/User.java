a5a67f7c951c87892cdc77143fee63190c1a80b5
Lorenzo <lmlory@hotmail.com>
2014-02-22 10:50:24 +0100

    no message

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entity;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Lorenzo
+ */
+@XmlRootElement
+public class User {
+    private String id;
+    private String name;
+    private String surname;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the surname
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * @param surname the surname to set
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
--- src/main/java/example/MyResource.java
+++ src/main/java/example/MyResource.java
@@ -1,9 +1,12 @@
 package example;
 
+import entity.User;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -12,8 +15,8 @@ import javax.ws.rs.core.MediaType;
 public class MyResource {
 
     /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
+     * Method handling HTTP GET requests. The returned object will be sent to
+     * the client as "text/plain" media type.
      *
      * @return String that will be returned as a text/plain response.
      */
@@ -21,5 +24,17 @@ public class MyResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
         return "Got it!";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("{name}/user")
+    public Response getUser(@PathParam("name") String name) {
+        User u = new User();
+        u.setId(Double.toString(Math.random()));
+        u.setName(name);
+        u.setSurname("test case");
+        Response res = Response.status(Response.Status.OK).entity(u).build();
+        return res;
     }
 }
